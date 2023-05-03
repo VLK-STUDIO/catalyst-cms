4f20f78295eb4a6da9ff73cbf4babd6ad3c93e00
Luca Cacciarru <luca.cacciarru@protonmail.com>
2023-05-03 23:05:28 +0200

    feat(core): Added field type select
    closes #29

--- examples/blog/src/cms.ts
+++ examples/blog/src/cms.ts
@@ -1,59 +1,73 @@
-import { createCatalyst } from "catalyst-cms";
+import { createCatalyst } from 'catalyst-cms';
 
 export const cms = createCatalyst({
   collections: {
     users: {
-      label: "Users",
+      label: 'Users',
       fields: {
         fullName: {
-          type: "text",
-          label: "Full name",
+          type: 'text',
+          label: 'Full name',
+        },
+        role: {
+          type: 'select',
+          label: 'Role',
+          options: [
+            {
+              label: 'Admin',
+              value: 'admin',
+            },
+            {
+              label: 'Editor',
+              value: 'editor',
+            },
+          ],
         },
       },
     },
     articles: {
-      previewUrl: "http://localhost:3000/:_id",
-      label: "Articles",
+      previewUrl: 'http://localhost:3000/:_id',
+      label: 'Articles',
       fields: {
         title: {
-          type: "text",
-          label: "Title",
+          type: 'text',
+          label: 'Title',
         },
         description: {
-          type: "text",
-          label: "Description",
+          type: 'text',
+          label: 'Description',
         },
         content: {
-          type: "richtext",
-          label: "Content",
+          type: 'richtext',
+          label: 'Content',
           localized: true,
         },
         author: {
-          type: "reference",
-          label: "Author",
-          collection: "users",
-          exposedColumn: "fullName",
+          type: 'reference',
+          label: 'Author',
+          collection: 'users',
+          exposedColumn: 'fullName',
         },
       },
     },
   },
   globals: {
     seo: {
-      label: "SEO",
+      label: 'SEO',
       fields: {
         title: {
-          type: "text",
-          label: "Title",
+          type: 'text',
+          label: 'Title',
         },
         description: {
-          type: "text",
-          label: "Description",
+          type: 'text',
+          label: 'Description',
         },
       },
     },
   },
   i18n: {
-    locales: ["en", "de"],
-    defaultLocale: "en",
+    locales: ['en', 'de'],
+    defaultLocale: 'en',
   },
 });
