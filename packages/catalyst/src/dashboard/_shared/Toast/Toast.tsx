69463b44ec623c252325ef59ccab7da92ea4ceb1
Luca Cacciarru <luca.cacciarru@protonmail.com>
2023-05-08 18:35:15 +0200

    feat(dashboard):Added toast creating/updating types
    closes #6

---
+++ packages/catalyst/src/dashboard/_shared/Toast/Toast.tsx
@@ -0,0 +1,46 @@
+import {
+  Icon,
+  IconAlertCircle,
+  IconCircleCheck,
+  IconCircleX,
+  IconInfoCircle
+} from "@tabler/icons-react";
+import { ToastType } from "./types";
+import clsx from "clsx";
+
+const toastTypeStyleMap: Record<ToastType, string> = {
+  error: "text-red-500",
+  success: "text-green-500",
+  warning: "text-yellow-500",
+  info: "text-blue-500"
+};
+
+const toastIconMap: Record<ToastType, Icon> = {
+  success: IconCircleCheck,
+  error: IconCircleX,
+  info: IconInfoCircle,
+  warning: IconAlertCircle
+};
+
+export type ToastProps = {
+  type: ToastType;
+  title: string;
+};
+
+export const Toast: React.FC<ToastProps> = ({ title, type }) => {
+  const selectedStyle = toastTypeStyleMap[type];
+  const SelectedToastIcon = toastIconMap[type];
+  return (
+    <div className="flex items-center gap-2 rounded-md   drop-shadow-lg">
+      <div
+        className={clsx(
+          "w-100 flex content-center items-center gap-2 text-lg text-black ",
+          selectedStyle
+        )}
+      >
+        <SelectedToastIcon />
+        {title}
+      </div>
+    </div>
+  );
+};
---
+++ packages/catalyst/src/dashboard/_shared/Toast/ToastProvider.tsx
@@ -0,0 +1,8 @@
+"use client";
+
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const ToastProvider: React.FC = () => {
+  return <ToastContainer />;
+};
